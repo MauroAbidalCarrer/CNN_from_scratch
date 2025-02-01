b02c7b79289d42f29ef729ba2d7f09edd6b7a89c
Mauro_Abidal_Carrer <mauro@bib-batteries.fr>
2025-02-01 14:16:01 +0100

    Feat: Updated losses.BinaryCrossentropy to work like MeanAbsoluteError.

--- src/losses.py
+++ src/losses.py
@@ -1,12 +1,13 @@
 import numpy as np
 
 class BinaryCrossentropy:
-    def forward(self, y_true: np.ndarray, y_pred: np.ndarray) -> float:
-        self.y_pred = np.clip(y_pred, 1e-7, 1 - 1e-7)
-        return -(y_true * np.log(self.y_pred) + (1 - y_true) * np.log(1 - self.y_pred))
+    def forward(self, y_pred: np.ndarray, y_true: np.ndarray) -> float:
+        y_pred = np.clip(y_pred, 1e-7, 1 - 1e-7)
+        return -(y_true * np.log(y_pred) + (1 - y_true) * np.log(1 - y_pred)).mean()
 
-    def backward(self, y_true: np.ndarray) -> np.ndarray:
-        return - (y_true / self.y_pred - (1 - y_true) / (1 - self.y_pred))
+    def backward(self, y_pred: np.ndarray, y_true: np.ndarray) -> np.ndarray:
+        y_pred = np.clip(y_pred, 1e-7, 1 - 1e-7)
+        return - (y_true / y_pred - (1 - y_true) / (1 - y_pred))
 
 class MeanAbsoluteError:
     def forward(self, y_pred: np.ndarray, y_true: np.ndarray) -> float:
