d095ffb3ee397fb53604ed097a5d46797e140ee6
Mauro_Abidal_Carrer <mauro@bib-batteries.fr>
2025-03-20 17:30:27 +0100

    Feat: Implemented SGD_with_moentum.

--- src/optimizers.py
+++ src/optimizers.py
@@ -21,14 +21,6 @@ class SGD:
         """Optimizes the neural network and returns a dataframe of the training metrics."""
         it = track(range(epochs), description="Training...") if use_track else range(epochs)
         for epoch in it:
-            # Perform steps
-            for batch_i in range(0, x.shape[0], batch_size):
-                batch_x = x[batch_i:batch_i+batch_size]
-                batch_y = y[batch_i:batch_i+batch_size]
-                batch_y_preds = SGD.forward(nn, batch_x)
-                batch_gradients = loss.backward(batch_y_preds, batch_y)
-                self.update_params(batch_gradients, nn)
-                self.iterations += 1
             # Compute and store new trainging metrics
             if not epoch % metric_freq:
                 y_pred = SGD.forward(nn, x)
@@ -39,6 +31,14 @@ class SGD:
                     "learning_rate": self.learning_rate,
                     **{score_func.__name__: score_func(y_pred, y) for score_func in score_funcs},
                 })
+            # Perform steps
+            for batch_i in range(0, x.shape[0], batch_size):
+                batch_x = x[batch_i:batch_i+batch_size]
+                batch_y = y[batch_i:batch_i+batch_size]
+                batch_y_preds = SGD.forward(nn, batch_x)
+                batch_gradients = loss.backward(batch_y_preds, batch_y)
+                self.update_params(batch_gradients, nn)
+                self.iterations += 1
         return DF.from_records(self.training_metrics)
 
     # Forward is implemented in SGD because it's the only place it's used, eventually it will be in a NueralNetwork module.
@@ -52,11 +52,11 @@ class SGD:
             inputs_grad = gradients.pop("inputs")
             for param_name, grads_wrt_param in gradients.items():
                 param:ndarray = getattr(layer, param_name)
-                gradient_wrt_param = self.postprocess_gradient(layer, param_name, grads_wrt_param)
+                gradient_wrt_param = self.postprocess_gradient(layer, param_name, param, grads_wrt_param)
                 setattr(layer, param_name, param - gradient_wrt_param)
             outputs_gradients = inputs_grad
 
-    def postprocess_gradient(self, layer:Layer, param_name:str, gradient_wrt_param:ndarray) -> ndarray:
+    def postprocess_gradient(self, layer:Layer, param_name:str, param:ndarray, gradient_wrt_param:ndarray) -> ndarray:
         return self.learning_rate * gradient_wrt_param
 
     @property
@@ -64,7 +64,7 @@ class SGD:
         return self._learning_rate
 
 class SGD_with_decay(SGD):
-    def __init__(self, starting_lr:float, lr_decay:float):
+    def __init__(self, starting_lr:float, lr_decay:float=0.0):
         self.starting_lr = starting_lr
         self.lr_decay = lr_decay
         super().__init__(starting_lr)
@@ -73,12 +73,13 @@ class SGD_with_decay(SGD):
     def learning_rate(self) -> float:
         return self.starting_lr / (1 + self.lr_decay * self.iterations)
     
-# class SGD_with_momentum(SGD_with_decay):
-#     def __init__(self, starting_lr: float, lr_decay: float, momentum:float):
-#         super().__init__(starting_lr, lr_decay)
-#         self.momentum = momentum
+class SGD_with_momentum(SGD_with_decay):
+    def __init__(self, starting_lr:float, lr_decay:float, momentum:float):
+        self.momentum = momentum
+        super().__init__(starting_lr, lr_decay)
 
-#     def compute_gradient_wrt_param(self, layer: Layer, param_name: str, gradients_wrt_param: ndarray) -> ndarray:
-#         momentum = getattr(layer, param_name + "_momentum", np.zeros_like(getattr(layer, param_name)))
-#         setattr(layer, param_name + "_momentum", momentum - gradients_wrt_param)
-#         return self.learning_rate * (momentum - gradients_wrt_param)
+    def postprocess_gradient(self, layer:Layer, param_name:str, param:ndarray, gradient_wrt_param:ndarray) -> ndarray:
+        param_momentum = getattr(layer, param_name + "_momentum", np.zeros_like(param))
+        post_processed_gradient_wrt_param = self.learning_rate * gradient_wrt_param + param_momentum * self.momentum
+        setattr(layer, param_name + "_momentum", post_processed_gradient_wrt_param)
+        return post_processed_gradient_wrt_param
