7e5ab4a21f93824315070180290983d868cd34d0
Mauro_Abidal_Carrer <mauro@bib-batteries.fr>
2025-03-25 16:07:25 +0100

    Feat: Refactored optimizer module to only have Adam as I will most likely never use the others, added the functionality to have FigureWidget updating live as the nn is being trained and more modular metric functions.

--- src/optimizers.py
+++ src/optimizers.py
@@ -1,78 +1,96 @@
 from functools import reduce, cache
 from IPython.display import display
+from dataclasses import dataclass, field
 
 import numpy as np
-from plotly.graph_objects import FigureWidget
-# from rich.progress import track
-from plotly.express import scatter
 from pandas import DataFrame as DF
-from numpy import ndarray, array_split
+from plotly.express import scatter
+from plotly.graph_objects import FigureWidget
+from numpy import ndarray, array_split as ndarray, split
 
 from losses import Loss
 from layers import Layer
 from metrics import accuracy
-from constants import EPSILON
+from constants import EPSILON, DFLT_X_PLT, DFLT_Y_PLT
 
 
-layers = list[Layer]
-
+@dataclass
 class Adam:
-    def __init__(self, starting_lr:float, lr_decay:float, momentum_weight:float, ada_grad_weight:float):
-        self.iterations = 0
-        self.training_metrics:list[dict] = []
-        self.momentum_weight = momentum_weight
-        self.ada_grad_weight = ada_grad_weight
-        self.starting_lr = starting_lr
-        self.lr_decay = lr_decay
+    nn:list[Layer]
+    x:ndarray
+    y:ndarray
+    loss:Loss
+    starting_lr:float
+    lr_decay:float
+    momentum_weight:float
+    ada_grad_weight:float
+    training_metrics:list[dict] = field(default_factory=list, init=False)
+    epoch: int = field(default=0, init=False)
+    iteration: int = field(default=0, init=False)
 
-    def optimize_nn(self, nn:layers, x:ndarray, y:ndarray, epochs:int, batch_size:int, loss:Loss, metric_freq:int=1, metrics=[accuracy], plt_x="epoch", plt_ys=["loss", "accuracy"]) -> DF:
+    def optimize_nn(self, epochs, batch_size, metric_freq=1, metrics=[accuracy], plt_x=None, plt_ys=None, **plt_kwargs) -> DF:
         """Optimizes the neural network and returns a dataframe of the training metrics."""
-        nb_batches = int(np.ceil(len(x) / batch_size))
-        for epoch in range(epochs):
-            if not epoch % metric_freq:
-                self.record_metrics(nn, x, y, epoch, metrics, loss)
-                if not plt_x is None:
-                    fig = self.get_figure_widget(plt_x, plt_ys)
-                    fig.data[0] = self.get_melted_training_metrics(plt_x, plt_ys)
-            for batch_x, batch_y in zip(array_split(x, nb_batches), array_split(y, nb_batches)):
-                self.step(nn, batch_x, batch_y, loss)
-        return DF.from_records(self.training_metrics)
+        try:
+            nb_batches = int(np.ceil(len(self.x) / batch_size))
+            fig = None
+            # Use self.epoch instead of epoch.
+            # This avoids resetting new metrics DF lines to the same epoch value in case this method gets recalled.
+            for _ in range(epochs):
+                if self.epoch % metric_freq == 0:
+                    self.record_metrics(metrics)
+                    if not plt_x is None:
+                        fig = self.create_figure_widget(plt_x, plt_ys, **plt_kwargs) if fig is None else fig
+                        self.update_figure(fig, plt_x, plt_ys)
+                # Shuffle x and y
+                permutation = np.random.permutation(len(self.x))
+                self.x = self.x[permutation]
+                self.y = self.y[permutation]
+                for batch_x, batch_y in zip(split(self.x, nb_batches), split(self.y, nb_batches)):
+                    self.step(batch_x, batch_y)
+                self.epoch += 1
+        except KeyboardInterrupt:
+            print("Caught KeyboardInterrupt exception, returning training metrics.")
+        finally:
+            return DF.from_records(self.training_metrics)
 
-    @cache
-    def get_figure_widget(self, plt_x, plt_ys) -> FigureWidget:
-        df = self.get_melted_training_metrics(plt_x, plt_ys)
-        fig = FigureWidget(scatter(df, plt_x, "value", facet_row="variable", color="varaible"))
+    def record_metrics(self, metric_funcs:list[callable]) -> dict[str, any]:
+        y_pred = self.forward(self.x)
+        self.training_metrics.append({
+            "iteration": self.iteration,
+            "epoch": self.epoch,
+            "loss": self.loss.forward(y_pred, self.y),
+            "learning_rate": self.learning_rate,
+            **{func.__name__: func(nn=self.nn, y_pred=y_pred, y_true=self.y, loss=self.loss) for func in metric_funcs}
+        })
+
+    def create_figure_widget(self, plt_x:str, plt_ys:list[str], **plt_kwargs) -> FigureWidget:
+        df = DF.from_records(self.training_metrics).melt(plt_x, plt_ys)
+        fig = scatter(df, plt_x, "value", facet_row="variable", color="variable", **plt_kwargs).update_yaxes(matches=None)
+        fig = FigureWidget(fig)
         display(fig)
         return fig
 
-    def get_melted_training_metrics(self, plt_x, plt_ys) -> DF:
-        return DF.from_records(self.training_metrics).melt(plt_x, plt_ys)
+    def update_figure(self, fig:FigureWidget, plt_x:str, plt_ys:list[str]):
+        df = DF.from_records(self.training_metrics)
+        with fig.batch_update():
+            for i, plt_y in enumerate(plt_ys):
+                fig.data[i].x = df[plt_x]
+                fig.data[i].y = df[plt_y]
 
-    def record_metrics(self, nn:layers, x:ndarray, y:ndarray, epoch:int, metric_funcs:list[callable], loss:Loss) -> dict[str, any]:
-        y_pred = forward(nn, x)
-        self.training_metrics.append({
-            "iteration": self.iterations,
-            "epoch": epoch,
-            "loss": loss.forward(y_pred, y),
-            "learning_rate": self.learning_rate,
-            **{func.__name__: func(nn=nn, y_pred=y_pred, y_true=y, loss=loss) for func in metric_funcs}
-        })
+    def step(self, batch_x:ndarray, batch_y:ndarray):
+        batch_y_preds = self.forward(batch_x)
+        batch_gradients = self.loss.backward(batch_y_preds, batch_y)
+        reduce(self.update_layer_params, reversed(self.nn), batch_gradients)
+        self.iteration += 1
 
-    def step(self, nn:layers, x:ndarray, y:ndarray, loss:Loss):
-        batch_y_preds = forward(nn, x)
-        batch_gradients = loss.backward(batch_y_preds, y)
-        self.update_params(batch_gradients, nn)
-        self.iterations += 1
-
-    def update_params(self, outputs_gradients:ndarray, nn:list[Layer]):
-        for layer in reversed(nn):
-            gradients:dict = layer.backward(outputs_gradients)
-            inputs_grad = gradients.pop("inputs")
-            for param_name, grads_wrt_param in gradients.items():
-                param:ndarray = getattr(layer, param_name)
-                gradient_wrt_param = self.postprocess_gradient(layer, param_name, grads_wrt_param)
-                setattr(layer, param_name, param - gradient_wrt_param)
-            outputs_gradients = inputs_grad
+    def update_layer_params(self, outputs_gradients:ndarray, layer:Layer) -> ndarray:
+        grads_wrt:dict = layer.backward(outputs_gradients)
+        grads_wrt_inputs = grads_wrt.pop("inputs")
+        for param_name, grad_wrt_param in grads_wrt.items():
+            param:ndarray = getattr(layer, param_name)
+            grad_wrt_param = self.postprocess_gradient(layer, param_name, grad_wrt_param)
+            setattr(layer, param_name, param - grad_wrt_param)
+        return grads_wrt_inputs
 
     def postprocess_gradient(self, layer:Layer, param_name:str, gradient_wrt_param:ndarray) -> ndarray:
         momentum = self.update_moving_average(layer, param_name + "_momentum", gradient_wrt_param, self.momentum_weight)
@@ -84,18 +102,18 @@ class Adam:
         moving_average = getattr(layer, moving_average_name, cached_zeros(gradient.shape)) # Get the moving averate
         moving_average = lerp(moving_average, gradient, beta) # Move the average
         setattr(layer, moving_average_name, moving_average) # Set the moved average for future use
-        return moving_average / (1 - beta ** (self.iterations + 1)) # Correct and return the average 
+        return moving_average / (1 - beta ** (self.iteration + 1)) # Correct and return the average 
 
     @property
     def learning_rate(self) -> float: 
-        return self.starting_lr / (1 + self.lr_decay * self.iterations)
+        return self.starting_lr / (1 + self.lr_decay * self.iteration)
+
+    # Forward is implemented in SGD because it's the only place it's used, eventually it will be in a NueralNetwork module.
+    def forward(self, inputs:ndarray) -> ndarray:
+        return reduce(lambda x, l: l.forward(x), self.nn, inputs)
 
 def lerp(a:ndarray, b:ndarray, t:float) -> ndarray:
     return a * t + (1 - t) * b
-
-# Forward is implemented in SGD because it's the only place it's used, eventually it will be in a NueralNetwork module.
-def forward(nn:list[Layer], inputs:ndarray) -> ndarray:
-    return reduce(lambda x, l: l.forward(x), nn, inputs)
 
 @cache
 def cached_zeros(shape) -> ndarray:
