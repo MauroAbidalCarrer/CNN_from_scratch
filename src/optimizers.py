b5110a865cae21aad8ac24e22072bb6e0c6ecd7d
Mauro_Abidal_Carrer <mauro@bib-batteries.fr>
2025-03-28 16:16:01 +0100

    Feat: made optimizer.Adam.optimize_nn overload to make the try/except of KeyboardInterreupt during training optional the method call itself is warped in a try/except KeyboardIntterrupt.

--- src/optimizers.py
+++ src/optimizers.py
@@ -1,5 +1,5 @@
 from itertools import accumulate
-from functools import reduce, cache
+from functools import reduce
 from IPython.display import display
 from dataclasses import dataclass, field
 
@@ -11,8 +11,9 @@ from numpy import ndarray, array_split as ndarray, split
 
 from losses import Loss
 from layers import Layer
-from metrics import metric_func
-from constants import EPSILON, DFLT_METRICS
+from metrics import metric_func, accuracy
+from constants import EPSILON
+from numpy_utils import cached_zeros
 
 
 @dataclass
@@ -29,39 +30,40 @@ class Adam:
     epoch: int = field(default=0, init=False)
     iteration: int = field(default=0, init=False)
 
-    def optimize_nn(self, epochs, batch_size, metric_freq=1, metrics:list[metric_func]=DFLT_METRICS, plt_x=None, plt_ys=None, **plt_kwargs) -> DF:
+    def optimize_nn(self, epochs, batch_size, metric_freq=1, metrics:list[metric_func]=[accuracy], catch_interrupt=False, plt_x=None, plt_ys=None, **plt_kwargs) -> DF:
         """Optimizes the neural network and returns a dataframe of the training metrics."""
-        try:
-            nb_batches = int(np.ceil(len(self.x) / batch_size))
-            fig = None
-            # Use self.epoch instead of epoch.
-            # This avoids resetting new metrics DF lines to the same epoch value in case this method gets recalled.
-            for _ in range(epochs):
-                if self.epoch % metric_freq == 0:
-                    self.record_metrics(metrics)
-                    if not plt_x is None:
-                        fig = self.create_figure_widget(plt_x, plt_ys, **plt_kwargs) if fig is None else fig
-                        self.update_figure(fig, plt_x, plt_ys)
-                # Shuffle x and y
-                permutation = np.random.permutation(len(self.x))
-                self.x = self.x[permutation]
-                self.y = self.y[permutation]
-                for batch_x, batch_y in zip(split(self.x, nb_batches), split(self.y, nb_batches)):
-                    self.step(batch_x, batch_y)
-                self.epoch += 1
-        except KeyboardInterrupt:
-            print("Caught KeyboardInterrupt exception, returning training metrics.")
+        if catch_interrupt:
+            try:
+                self.optimize_nn(epochs, batch_size, metric_freq, metrics, plt_x, plt_ys, **plt_kwargs)
+            except KeyboardInterrupt:
+                print("Caught KeyboardInterrupt exception, returning training metrics.")
+        else:
+            self.optimize_nn(epochs, batch_size, metric_freq, metrics, plt_x, plt_ys, **plt_kwargs)
         return DF.from_records(self.training_metrics)
+
+    def optimize_nn(self, epochs, batch_size, metric_freq=1, metrics:list[metric_func]=[accuracy], plt_x=None, plt_ys=None, **plt_kwargs) -> DF:
+        nb_batches = int(np.ceil(len(self.x) / batch_size))
+        fig = None
+        # Use self.epoch instead of epoch.
+        # This avoids resetting new metrics DF lines to the same epoch value in case this method gets recalled.
+        for _ in range(epochs):
+            if self.epoch % metric_freq == 0:
+                self.record_metrics(metrics)
+                if not plt_x is None:
+                    fig = self.create_figure_widget(plt_x, plt_ys, **plt_kwargs) if fig is None else fig
+                    self.update_figure(fig, plt_x, plt_ys)
+            # Shuffle x and y
+            permutation = np.random.permutation(len(self.x))
+            self.x = self.x[permutation]
+            self.y = self.y[permutation]
+            for batch_x, batch_y in zip(split(self.x, nb_batches), split(self.y, nb_batches)):
+                self.step(batch_x, batch_y)
+            self.epoch += 1
 
     def record_metrics(self, metric_funcs:list[callable]) -> dict[str, any]:
         activations = self.forward(self.x)
         y_pred = activations[-1]
-        gradients = list(accumulate(
-            reversed(self.nn), 
-            lambda g, l: l.backward(g)["inputs"],
-            initial=self.loss.backward(y_pred, self.y)
-        ))
-        metric_kwargs = dict(nn=self.nn, activations=activations, y_pred=y_pred, y_true=self.y, loss=self.loss, gradients=gradients)
+        metric_kwargs = dict(nn=self.nn, activations=activations, y_pred=y_pred, y_true=self.y, loss=self.loss)
         new_metric_line = reduce(
             lambda metric_line, metric_func: metric_func(metric_line, **metric_kwargs),
             metric_funcs,
@@ -123,11 +125,6 @@ class Adam:
     # Forward is implemented in SGD because it's the only place it's used, eventually it will be in a NueralNetwork module.
     def forward(self, inputs:ndarray) -> list[ndarray]:
         return list(accumulate(self.nn, lambda x, l: l.forward(x), initial=inputs))
-        # return reduce(lambda x, l: l.forward(x), self.nn, inputs)
 
 def lerp(a:ndarray, b:ndarray, t:float) -> ndarray:
     return a * t + (1 - t) * b
-
-@cache
-def cached_zeros(shape) -> ndarray:
-    return np.zeros(shape)
