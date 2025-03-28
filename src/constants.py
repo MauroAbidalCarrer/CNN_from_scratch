2df35c7890e65f2bd5c6225c58604f6167813b92
Mauro_Abidal_Carrer <mauro@bib-batteries.fr>
2025-03-28 16:23:20 +0100

    Feat: Added new metric functions.

--- src/constants.py
+++ src/constants.py
@@ -1,6 +1,4 @@
-from metrics import accuracy
-
 DEFAULT_WEIGHTS_SCALING = 0.001
 # Tiny positive number for numerical stability in divisions
 EPSILON = 1e-7
-DFLT_METRICS = [accuracy]
+PARAM_NAMES = ["weights", "biases", "kernels"]
