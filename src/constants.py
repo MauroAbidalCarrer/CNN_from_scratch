6b8b97f424a5ba993adf41270c61997c2f675c35
Mauro Abidal Carrer <mauroabida@yahoo.fr>
2025-04-10 16:41:13 +0200

    Wip: Tried new MAX_NB_SAMPLES value.

--- src/constants.py
+++ src/constants.py
@@ -2,4 +2,5 @@ DEFAULT_WEIGHTS_SCALING = 0.001
 # Tiny positive number for numerical stability in divisions
 EPSILON = 1e-7
 PARAM_NAMES = ["weights", "biases", "kernels"]
-MAX_NB_SAMPLES = int(1e4)
+MAX_NB_SAMPLES = 500
+DFLT_NEGATIVE_LEAKY_RELU_SLOPE = 1e-2 # Taken from pytorch's default 
